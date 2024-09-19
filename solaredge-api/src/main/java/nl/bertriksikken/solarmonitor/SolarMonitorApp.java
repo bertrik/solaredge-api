1ae730ac0f98c2fcc544f58d29eb093a46e61339
Bertrik Sikken <bertrik@sikken.nl>
2024-09-19 18:44:57 +0200

    Use error prone instead of spotbugs.

--- solaredge-api/src/main/java/nl/bertriksikken/solarmonitor/SolarMonitorApp.java
+++ solaredge-api/src/main/java/nl/bertriksikken/solarmonitor/SolarMonitorApp.java
@@ -1,12 +1,8 @@
 package nl.bertriksikken.solarmonitor;
 
 import org.apache.log4j.PropertyConfigurator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class SolarMonitorApp {
-
-    private static final Logger LOG = LoggerFactory.getLogger(SolarMonitorApp.class);
 
     public static void main(String[] args) {
         PropertyConfigurator.configure("log4j.properties");
