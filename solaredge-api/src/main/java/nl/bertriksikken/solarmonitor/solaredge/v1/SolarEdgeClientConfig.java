5e87c88416d3ddeed28620b37b99372ef5bcf133
Bertrik Sikken <bertrik@sikken.nl>
2025-08-03 15:01:41 +0200

    Update libraries and plugins

--- solaredge-api/src/main/java/nl/bertriksikken/solarmonitor/solaredge/v1/SolarEdgeClientConfig.java
+++ solaredge-api/src/main/java/nl/bertriksikken/solarmonitor/solaredge/v1/SolarEdgeClientConfig.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.Duration;
 
-@JsonIgnoreProperties()
+@JsonIgnoreProperties
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
 public final class SolarEdgeClientConfig {
 
